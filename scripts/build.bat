764bf6e9a5cb675991617065d4d576c845577b5b
Dirker Dero <dirkerdero@proton.me>
2024-06-25 17:18:40 +0000

    Fix BAT files while we are here

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,9 +1,16 @@
+@echo on
 
 mkdir .\build
 pushd .\build
 
 dir c:\mingw64\bin\
-c:\mingw64\bin\cmake.exe ..
+
+if "%1%" == "ci" (
+  c:\mingw64\bin\cmake.exe -DCMAKE_MAKE_PROGRAM=c:/mingw64/bin/ninja.exe -G "Ninja" ..
+) else (
+  c:\mingw64\bin\cmake.exe ..
+)
+
 c:\mingw64\bin\ninja.exe
 
 popd
